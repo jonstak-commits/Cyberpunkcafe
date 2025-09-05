1a138d0c42a7a156505d48355a49475dfb084162
jonstak-commits <jonstak@mail.ru>
2025-09-05 10:24:52 +0300

    Create Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar';
+export default function Layout({ children }) {
+return (
+<>
+<navbar>
+<main padding: '20px' }}>{children}</main>
+{/* футер можно добавить позже */}
+</>
+);
+}
