9bb011843f6cf9877844ccc33a0b8b54d84adb3c
jonstak-commits <jonstak@mail.ru>
2025-09-05 10:29:11 +0300

    Create Card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Card({ product }){
+return (
+<div width:240, border:'1px solid #2a2a2a', borderradius:12, padding:12, background:'#141625' }}>
+<div height:140, background:'#222', borderradius:8, marginbottom:8 }}>
+<div fontweight:700 }}>{product.name}</div>
+<div color:'#aaa' }}>{product.price} ₽</div>
+</div>
+);
+}
